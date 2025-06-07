72ef66c8e122a333cfd09548359cf3ad2831990a
Aline.mouzinho <aline.mouzinho.ext@synchro.com.br>
2025-06-06 23:05:23 -0300

    #Commit teste

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,14 @@
-module demo { // O nome do seu módulo. Mantenha 'demo' se não mudou.
+module demo { 
     requires javafx.controls;
     requires javafx.fxml;
-    // requires javafx.graphics; // Mantenha se estiver usando ícones ou outros gráficos
 
-    // Pacote do menu (MainApplication e MenuController)
-    opens menu to javafx.fxml; // Permite que o FXMLLoader acesse classes do pacote 'menu'
-    exports menu;                // Exporta o pacote 'menu' para ser visível a outros módulos
+    opens menu to javafx.fxml; 
+    exports menu;                
 
-    // Pacote do jogo (GameController)
-    opens transicao1 to javafx.fxml;   // Permite que o FXMLLoader acesse classes do pacote 'transicao1'
-    exports transicao1;                // Exporta o pacote 'transicao1'
+  
+    opens transicao1 to javafx.fxml;   
+    exports transicao1;                
 
 
 }
+//testeAline
